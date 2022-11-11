d1c3d54b971dfec932e62d4642f434e1af1050d5
Orlando Cano <isc.orlando.cano@gmail.com>
2022-11-10 22:54:56 -0600

    latest changes for demo

--- src/main/kotlin/com/hello/reactive/model/Entities.kt
+++ src/main/kotlin/com/hello/reactive/model/Entities.kt
@@ -1,9 +1,10 @@
 package com.hello.reactive.model
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 import java.util.Date
 
-
+@Document("books_kt")
 data class Book (
         @Id var id: String?,
         var isbn: String,
